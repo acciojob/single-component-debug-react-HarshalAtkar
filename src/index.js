ebda220311db9e94ae4a27b40c3aa9fac820a87f
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2024-05-01 17:56:31 +0000

    added solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-
-
+const root=ReactDOM.createRoot(document.getElementbyId("root"));
+root.render(<App/>);
